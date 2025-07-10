19641b423253be5c4c6f028bebb2062fbe74b512
Bloodyly <155741658+Bloodyly@users.noreply.github.com>
2025-07-10 11:15:24 +0200

    final?

--- i2cread_axp.c
+++ i2cread_axp.c
@@ -40,8 +40,6 @@ int main(int argc, char **argv) {
 
     print_header();
 
-    int currents[] = { 100, 190, 280, 360, 450, 550, 630, 700 };
-
     int reg00 = read_reg(file, addr, 0x00);
     print_row("PMU Status (0x00)", reg00, "-");
 
@@ -51,23 +49,25 @@ int main(int argc, char **argv) {
     int acv = ((read_reg(file, addr, 0x56) << 4) | (read_reg(file, addr, 0x57) & 0x0F));
     int acc = ((read_reg(file, addr, 0x58) << 4) | (read_reg(file, addr, 0x59) & 0x0F));
     char acvolt[32], accurr[32];
-    snprintf(acvolt, sizeof(acvolt), "%dmV", (int)(acv * 17 / 10));
-    snprintf(accurr, sizeof(accurr), "%dmA", (int)(acc * 625 / 1000));
+    snprintf(acvolt, sizeof(acvolt), "%dmV", (int)(acv * 1.7));
+    snprintf(accurr, sizeof(accurr), "%dmA", (int)(acc * 0.625));
     print_row("ACIN Voltage (0x56/0x57)", acv, acvolt);
     print_row("ACIN Current (0x58/0x59)", acc, accurr);
 
     int vbv = ((read_reg(file, addr, 0x5A) << 4) | (read_reg(file, addr, 0x5B) & 0x0F));
     int vbc = ((read_reg(file, addr, 0x5C) << 4) | (read_reg(file, addr, 0x5D) & 0x0F));
     char vbvolt[32], vbcurr[32];
-    snprintf(vbvolt, sizeof(vbvolt), "%dmV", (int)(vbv * 17 / 10));
-    snprintf(vbcurr, sizeof(vbcurr), "%dmA", (int)(vbc * 375 / 1000));
+    snprintf(vbvolt, sizeof(vbvolt), "%dmV", (int)(vbv * 1.7));
+    snprintf(vbcurr, sizeof(vbcurr), "%dmA", (int)(vbc * 0.375));
     print_row("VBUS Voltage (0x5A/0x5B)", vbv, vbvolt);
     print_row("VBUS Current (0x5C/0x5D)", vbc, vbcurr);
 
     int reg33 = read_reg(file, addr, 0x33);
+    int charge_idx = (reg33 >> 4) & 0x7;
+    int charge_mA[] = { 100, 190, 280, 360, 450, 550, 630, 700 };
     char charge_str[64];
     snprintf(charge_str, sizeof(charge_str), "%s (%dmA)",
-             (reg33 & 0x80) ? "Enabled" : "Disabled", currents[(reg33 >> 4) & 0x7]);
+             (reg33 & 0x80) ? "Enabled" : "Disabled", charge_mA[charge_idx]);
     print_row("Charge Control 1 (0x33)", reg33, charge_str);
 
     int reg34 = read_reg(file, addr, 0x34);
@@ -92,23 +92,28 @@ int main(int argc, char **argv) {
     int reg7B = read_reg(file, addr, 0x7B);
     int vbat_raw = ((reg7A << 4) | (reg7B & 0x0F));
     char vbat_info[32];
-    snprintf(vbat_info, sizeof(vbat_info), "%dmV", (int)(vbat_raw * 11 / 10));
+    snprintf(vbat_info, sizeof(vbat_info), "%dmV", (int)(vbat_raw * 1.1));
     print_row("Battery Voltage (0x7A/0x7B)", vbat_raw, vbat_info);
 
     int reg78 = read_reg(file, addr, 0x78);
     char fuel_str[32];
-    snprintf(fuel_str, sizeof(fuel_str), "Fuel Gauge Control: 0x%02X", reg78);
+    snprintf(fuel_str, sizeof(fuel_str), "Fuel Gauge Ctrl: 0x%02X", reg78);
     print_row("Fuel Gauge Control (0x78)", reg78, fuel_str);
 
-    int charge_current = ((read_reg(file, addr, 0x7E) << 4) | (read_reg(file, addr, 0x7F) & 0x0F));
+    int charge_current = ((read_reg(file, addr, 0x7E) << 5) | (read_reg(file, addr, 0x7F) & 0x1F));
     char chgcurr[32];
     snprintf(chgcurr, sizeof(chgcurr), "%dmA", charge_current);
     print_row("Charge Current (0x7E/0x7F)", charge_current, chgcurr);
 
-    for (int r = 0xB9; r <= 0xBE; r++) {
+    int regB9 = read_reg(file, addr, 0xB9);
+    char soc_str[32];
+    snprintf(soc_str, sizeof(soc_str), "%d%%", regB9 & 0x7F);
+    print_row("Fuel Gauge (0xB9)", regB9, soc_str);
+
+    for (int r = 0xBA; r <= 0xBE; r++) {
         int val = read_reg(file, addr, r);
         char desc[64];
-        snprintf(desc, sizeof(desc), "Temperature/Other Register (0x%02X)", r);
+        snprintf(desc, sizeof(desc), "Status Register (0x%02X)", r);
         print_row(desc, val, "-");
     }
 
